4aecde246f77730c90fb48932172f5602fa7820f
tat023 <122314613+tat023@users.noreply.github.com>
2023-07-31 14:38:53 -0700

    Add files via upload

---
+++ SaleScrapper.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+url_name = "https://news.search.yahoo.com/search;_ylt=AwrO6mi2JcRkrHcLDjPQtDMD;_ylu=Y29sbwNncTEEcG9zAzEEdnRpZAMEc2VjA3BhZ2luYXRpb24-?p=shopping+clearance+sale&pz=10&fr=uh3_news_web&fr2=sb-top&bct=0&b=1&pz=10&bct=0&xargs=0"
+test_page = requests.get(url_name)
+soup = BeautifulSoup(test_page.text, "html.parser")
+key_word = ['close', 'closure', 'closing', 'nordstrom', 'target']
+link_names = soup.find_all('a')
+if test_page.status_code == 200:
+    for link in link_names:
+        link_text = link.get_text()
+        for word in key_word:
+            if word.lower() in link_text.lower():
+                url = link.get('href')
+                print(url)
+
+
+
