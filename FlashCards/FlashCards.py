be5aab6bd8639bebf207485b0bf7918897420073
tat023 <terrieatran@gmail.com>
2023-08-15 14:01:08 -0700

    List is converted to a dictionary

--- FlashCards/FlashCards.py
+++ FlashCards/FlashCards.py
@@ -1,42 +1,31 @@
-sec_terms = {'card': 'def'}
-print(sec_terms)
-print(sec_terms['card'])
-dictionary = dict(term = 'CI', definition = 'continuous integration')
-print(dictionary)
-print(dictionary.keys())
-
-my_list_keys = ['term', 'works']
-my_list_values = ['the def', 'answer is yes']
-big_list = ['this', 'is', 'working', 'h', 'another', 'wow', 'six']
-start_dict = {}
-#for iteration in range(0, len(big_list), 2):
- #   start_dict[big_list[iteration]] = big_list[iteration+1]
-#print(start_dict)
-
-for i in range(0, len(big_list), 3):
-    print(i)
-    print(big_list[i])
-
-
-
-
 class Term:
     """Represents a list of terms.  This list will be converted to a dictionary"""
     def __init__(self, list):
         self._list = list
+        self._dict = {}
     def convert_list(self):
         """Returns a dictionary based on the list passed into the Term object"""
+        for i in range(0, len(self._list), 2):
+            self._dict[self._list[i]] = self._list[i + 1]
+        return self._dict
     def add_terms(self):
-        """Returns nothing, only adds a term and a definition to the dictionary"""
+        """Returns nothing, only adds a list of terms and definitions to the dictionary"""
     def delete_term(self):
-        """Returns nothing, deletes a term and a definition from the dictionary"""
+        """Returns nothing, deletes a list of terms and definitions from the dictionary"""
     def get_dictionary(self):
-        """Returns to dictionary and any updates that may have been made"""
+        """Returns and prints the dictionary and any updates that may have been made"""
+        print(self._dict)
+        return self._dict
+
 
 def main():
-    list = [1, 2, 3]
+    list = ['Continuous integration (CI)', 'Code is constantly written and merged into a central repo frequently',
+            'Security checks CI', 'Documented sec baselines, large scale security analysis during testing',
+            'Continuous Delivery (CD)', 'the testing and release is automated for the application, manually deploy app',
+            'Continuous Deployment (CD)', 'Entire testing and deployment process is automated']
     ob = Term(list)
-    print(ob)
+    ob.convert_list()
+    ob.get_dictionary()
 if __name__ == '__main__':
     main()
 
